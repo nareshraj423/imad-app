e9cff473eeecfce1419e9c3c756faec631f5b2a6
Naresh Raj G <nareshbond23@gmail.com>
2017-11-02 16:37:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter', function (req,res) {
     counter += 1;
-    res.send(counter);
+    res.send();
 });
 
 app.get('/ui/style.css', function (req, res) {
