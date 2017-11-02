66934c046cfb93ab7f99ec4a3651a81f92de85d3
Naresh Raj G <nareshbond23@gmail.com>
2017-11-02 16:33:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var counter = 0;
 app.get('/counter', function (req,res) {
-    counter+=1;
+    counter += 1;
 });
 
 app.get('/ui/style.css', function (req, res) {
